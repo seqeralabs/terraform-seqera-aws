125c20d6682fbc221ec5f29528b38b7a1e77a834
enekui <mail@adiannyramirez.com>
2023-08-03 13:58:12 +0200

    Fixed memorydb users name

--- variables.tf
+++ variables.tf
@@ -592,13 +592,13 @@ variable "redis_users" {
   description = "A map of Redis user configurations."
   default = {
     admin = {
-      user_name     = "tower_admin-user"
+      user_name     = "tower-admin-user"
       access_string = "on ~* &* +@all"
       passwords     = ["YouShouldPickAStrongSecurePassword987!"]
       tags          = { User = "admin" }
     }
     readonly = {
-      user_name     = "tower_readonly-user"
+      user_name     = "tower-readonly-user"
       access_string = "on ~* &* -@all +@read"
       passwords     = ["YouShouldPickAStrongSecurePassword123!"]
       tags          = { User = "readonly" }
