e7dd550e8d8559fd379b71d3fb443f3ddfb84a06
enekui <mail@adiannyramirez.com>
2023-08-09 15:22:40 +0200

    Fixing role name output.

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "tower_irsa_iam_role_name" {
   description = "The name of the IAM role created for the Tower service account"
-  value       = try(module.terraform-seqera-module.module.eks.aws_iam_role.this[0].name, null)
+  value       = try(module.eks.aws_iam_role.this[0].name, null)
 }
